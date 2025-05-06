b4193fdd9affa9742eeed7094255a325f8952d70
Luis Colunga Perez <perez.luis.colunga@gmail.com>
2025-05-05 18:50:20 -0600

    ADD: Add footer component

--- src/lib/i18n/ui.ts
+++ src/lib/i18n/ui.ts
@@ -14,6 +14,10 @@ export const ui = {
     "header.products": "Productos",
     "header.contact": "Contacto",
     "header.legals": "Legales",
+    "footer.copyright":
+      "© 2025 Capital Invester. Todos los derechos reservados.",
+    "footer.terms":
+      "Capital Invester no garantiza resultados ni rendimientos específicos. Toda inversión conlleva riesgos, incluidos la pérdida parcial o total del capital. Antes de operar, asegúrate de comprender los riesgos involucrados y consulta con un asesor financiero independiente si es necesario. La información proporcionada en esta plataforma tiene fines informativos y educativos, y no debe interpretarse como una recomendación de inversión.",
   },
   en: {
     "header.home": "Home",
@@ -21,5 +25,9 @@ export const ui = {
     "header.products": "Products",
     "header.contact": "Contact",
     "header.legals": "Legals",
+    "footer.copyright":
+      "© 2025 Capital Invester. Todos los derechos reservados.",
+    "footer.terms":
+      "Capital Invester no garantiza resultados ni rendimientos específicos. Toda inversión conlleva riesgos, incluidos la pérdida parcial o total del capital. Antes de operar, asegúrate de comprender los riesgos involucrados y consulta con un asesor financiero independiente si es necesario. La información proporcionada en esta plataforma tiene fines informativos y educativos, y no debe interpretarse como una recomendación de inversión.",
   },
 } as const;
