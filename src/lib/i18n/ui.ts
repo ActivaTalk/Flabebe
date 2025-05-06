a231900ae9575fdc5b69e7b06da96ca2c783b57a
Luis Colunga Perez <perez.luis.colunga@gmail.com>
2025-05-05 18:14:10 -0600

    ADD: Add i18n functions

---
+++ src/lib/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const showDefaultLang = false;
+
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "header.home": "Inicio",
+    "header.forex": "Forex",
+    "header.products": "Productos",
+    "header.contact": "Contacto",
+    "header.legals": "Legales",
+  },
+  en: {
+    "header.home": "Home",
+    "header.forex": "Forex",
+    "header.products": "Products",
+    "header.contact": "Contact",
+    "header.legals": "Legals",
+  },
+} as const;
---
+++ src/utils/links.ts
@@ -0,0 +1,15 @@
+export enum LinksES {
+  HOME = "/",
+  FOREX = "/forex",
+  PRODUCTS = "/productos",
+  CONTACT = "/contacto",
+  LEGALS = "/legales",
+}
+
+export enum LinksEn {
+  HOME = "/",
+  FOREX = "/forex",
+  PRODUCTS = "/products",
+  CONTACT = "/contact",
+  LEGALS = "/legals",
+}
